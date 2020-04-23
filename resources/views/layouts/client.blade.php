64c7d179af238f2a204c8fb4a8cfc63f5e8372dc
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-04-23 15:48:57 -0500

    first commit

---
+++ resources/views/layouts/client.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+      <header>
+        <nav class="row nav-client py-4">
+          <div class="container">
+
+          </div>
+          <img src="{{asset('images/logo-horizontal.png')}}" class="img-fluid col-4" alt="">
+          <p>Formulario para inscripciones</p>
+        </nav>
+      </header>
+        <main class="py-4">
+            @yield('content')
+        </main>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </div>
+</body>
+</html>
